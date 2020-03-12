b563a6d2b0556d75ba63a816f13c5c3e29b4aebb
shibayaman <shibayaman7374@gmail.com>
2020-03-13 00:44:06 +0900

    Add TypeScript

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+console.log(1, 5);
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = env => ({
   mode: env.NODE_ENV || 'production',
-  entry: './index.js',
+  entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build'),
